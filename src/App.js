fa41193bce7c65f27e13ddb1989cd23a5aa4930f
Si Thu Win <sthuwin@gmail.com>
2021-10-06 21:40:08 +0800

    added register complete page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import RegisterComplete from "./pages/auth/RegisterComplete";
 import Home from "./pages/Home";
 import Header from "./components/nav/Header";
 
@@ -15,6 +16,7 @@ const App = () => {
       <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/register/complete" component={RegisterComplete} />
         <Route exact path="/" component={Home} />
       </Switch>
       <ToastContainer />

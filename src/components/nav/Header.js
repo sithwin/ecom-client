aa16c3d5dcaa0af529c7224e23d46c0e287f0a37
Si Thu Win <sthuwin@gmail.com>
2021-10-11 23:07:57 +0800

    login

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,20 +1,37 @@
 import React, { useState } from "react";
 import { Menu } from "antd";
-import { Link } from "react-router-dom";
 import {
-  UserOutlined,
-  UserAddOutlined,
   AppstoreOutlined,
   SettingOutlined,
+  UserOutlined,
+  UserAddOutlined,
+  LogoutOutlined,
 } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import firebase from "firebase";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 const { SubMenu, Item } = Menu;
 
 const Header = () => {
-  const [current, setCurrent] = useState("home");
+  const [current, setCurrent] = useState("login");
+
+  let dispatch = useDispatch();
+  let history = useHistory();
 
   const handleClick = (e) => {
+    // console.log(e.key);
     setCurrent(e.key);
+  };
+
+  const logout = () => {
+    firebase.auth().signOut();
+    dispatch({
+      type: "LOGOUT",
+      payload: null,
+    });
+    history.push("/login");
   };
 
   return (
@@ -22,21 +39,22 @@ const Header = () => {
       <Item key="home" icon={<AppstoreOutlined />}>
         <Link to="/">Home</Link>
       </Item>
-      <SubMenu
-        key="SubMenu"
-        icon={<SettingOutlined />}
-        className="float-right"
-        title="Username"
-      >
-        <Item key="setting:1">Option 1</Item>
-        <Item key="setting:2">Option 2</Item>
-      </SubMenu>
+
       <Item key="register" icon={<UserAddOutlined />} className="float-right">
         <Link to="/register">Register</Link>
       </Item>
+
       <Item key="login" icon={<UserOutlined />} className="float-right">
         <Link to="/login">Login</Link>
       </Item>
+
+      <SubMenu icon={<SettingOutlined />} title="Username">
+        <Item key="setting:1">Option 1</Item>
+        <Item key="setting:2">Option 2</Item>
+        <Item icon={<LogoutOutlined />} onClick={logout}>
+          Logout
+        </Item>
+      </SubMenu>
     </Menu>
   );
 };
--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,9 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "antd";
+import { MailOutlined } from "@ant-design/icons";
+import { auth } from "../../firebase";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
 
-const Login = () => {
+const Login = ({ history }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  let dispatch = useDispatch();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const result = auth.signInWithEmailAndPassword(email, password);
+      console.log(result);
+      const { user } = result;
+      const idTokenResult = await user.getIdTokenResult();
+
+      dispatch({
+        type: "LOGGED_IN_USER",
+        payload: {
+          email: user.email,
+          token: idTokenResult.token,
+        },
+      });
+      history.pushState("/");
+    } catch (error) {
+      //
+      console.log("ERROR  ", error);
+      toast.error(error.message);
+      setLoading(false);
+    }
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <input
+          type="email"
+          className="form-control"
+          onChange={(e) => setEmail(e.target.value)}
+          valid={email}
+          autoFocus
+          placeholder="user name"
+        />
+      </div>
+      <div className="form-group">
+        <input
+          type="password"
+          className="form-control"
+          onChange={(e) => setPassword(e.target.value)}
+          valid={password}
+          placeholder="password"
+        />
+      </div>
+
+      <br />
+      <Button
+        onClick={handleSubmit}
+        type="primary"
+        className="mb-3"
+        block
+        shape="round"
+        icon={<MailOutlined />}
+        size="large"
+        disabled={!email || password.length < 6}
+      >
+        Login with email/ password
+      </Button>
+    </form>
+  );
+
   return (
-    <div>
-      <p>Login</p>
+    <div className="container p-5">
+      <div className="col-md-6 offset-md-3">
+        <h4>Login</h4>
+        {loginForm()}
+      </div>
     </div>
   );
 };

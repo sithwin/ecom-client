2e5ffbb4ca4209d2b661374754155ee727b015e1
Si Thu Win <sthuwin@gmail.com>
2021-10-27 22:10:35 +0800

    set password

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ import ForgotPassword from "./pages/auth/ForgotPassword";
 import { currentUser } from "./functions/auth";
 import History from "./pages/user/History";
 import UserRoute from "./components/routes/UserRoute";
+import Password from "./pages/user/Password";
+import Wishlist from "./pages/user/Wishlist";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -57,6 +59,8 @@ const App = () => {
         <Route exact path="/register/complete" component={RegisterComplete} />
         <Route exact path="/forgot/password" component={ForgotPassword} />
         <UserRoute exact path="/user/history" component={History} />
+        <UserRoute exact path="/user/password" component={Password} />
+        <UserRoute exact path="/user/wishlist" component={Wishlist} />
       </Switch>
     </>
   );
